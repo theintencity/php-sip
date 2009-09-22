4ceea022c98dcf7dce831d6b97a599bb0945c6b3
chris.maciejewski <chris.maciejewski@795e483e-940a-11de-9a8c-5f207592b8ba>
2009-09-22 20:05:40 +0000

    we need full sip URI

--- trunk/example.php
+++ trunk/example.php
@@ -3,17 +3,17 @@ require_once('PhpSIP.class.php');
 
 $api = new PhpSIP('192.168.5.10'); // IP we will bind to
 $api->setUsername('10000'); // authentication username
-$api->setPassword('test'); // authentication password
+$api->setPassword('secret'); // authentication password
 // $api->setProxy('some_ip_here'); 
 $api->addHeader('Event: resync');
 $api->setMethod('NOTIFY');
-$api->setFrom('10000@sip.domain.com');
-$api->setUri('10000@sip.domain.com');
+$api->setFrom('sip:10000@sip.domain.com');
+$api->setUri('sip:10000@sip.domain.com');
 
 try
 {
   $res = $api->send();
-  echo "res1: $res\n";
+  echo "response: $res\n";
   
 } catch (Exception $e) {
   
