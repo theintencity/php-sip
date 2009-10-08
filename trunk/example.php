dc3993393c3b0cc71828ccdd56caabbbb51c4831
chris.maciejewski <chris.maciejewski@795e483e-940a-11de-9a8c-5f207592b8ba>
2009-10-08 11:17:00 +0000

    small change

--- trunk/example.php
+++ trunk/example.php
@@ -1,18 +1,18 @@
 <?php
 require_once('PhpSIP.class.php');
 
-$api = new PhpSIP('192.168.5.10'); // IP we will bind to
-$api->setUsername('10000'); // authentication username
-$api->setPassword('secret'); // authentication password
-// $api->setProxy('some_ip_here'); 
-$api->addHeader('Event: resync');
-$api->setMethod('NOTIFY');
-$api->setFrom('sip:10000@sip.domain.com');
-$api->setUri('sip:10000@sip.domain.com');
-
 try
 {
+  $api = new PhpSIP('192.168.5.10'); // IP we will bind to
+  $api->setUsername('10000'); // authentication username
+  $api->setPassword('secret'); // authentication password
+  // $api->setProxy('some_ip_here'); 
+  $api->addHeader('Event: resync');
+  $api->setMethod('NOTIFY');
+  $api->setFrom('sip:10000@sip.domain.com');
+  $api->setUri('sip:10000@sip.domain.com');
   $res = $api->send();
+
   echo "response: $res\n";
   
 } catch (Exception $e) {
